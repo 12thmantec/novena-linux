6fb25f7cad465ecfbb62d3ae81b171ce7b8c870e
Will <niutao0602@163.com>
2014-11-16 23:04:55 +0800

    update the copyright

--- drivers/char/novds_gpio_driver.h
+++ drivers/char/novds_gpio_driver.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2012 will.niu@pitayatech.com
+ *   Copyright (C) 2014 12thmantec
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
